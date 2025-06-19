cffd1f0c7cbee5779c63cb21de27f34daf0d9e27
earvin-tech <wsl@Earvin>
2025-06-19 16:08:00 +1000

    fix: updated routes with validation middlware for user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,19 +11,21 @@ const requireAuth = require("../middleware/requireAuth");
 const {
     validateRegister,
     validatePasswordUpdate,
+    validateLogin,
+    validateDeleteUser
 } = require("../middleware/validateUserInput");
 
 // POST Register
 router.post("/register", validateRegister, registerUser);
 
 // POST Login
-router.post("/login", loginUser);
+router.post("/login", validateLogin, loginUser);
 
 // PATCH Update Password
 router.patch("/update-password", requireAuth, validatePasswordUpdate, updateUserPassword);
 
 // DELETE
-router.delete("/", requireAuth, deleteUser)
+router.delete("/", requireAuth, validateDeleteUser, deleteUser)
 
 // GET Get current user details (email, username only)
 router. get("/me", requireAuth, getCurrentUser);
